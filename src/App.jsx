83136f49ca033a26bf418635cdef013ee69eaeeb
Ido <Idosabach11@gmail.com>
2024-01-11 20:48:41 +0200

    add version learn

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,16 @@ function CoreConcept({image , title , description}){
   );
 }
 
+// function CoreConcept(props){
+//   return (
+//     <li>
+//       <img src={props.image} alt={props.title} />
+//       <h3>{props.title}</h3>
+//       <p>{props.description}</p>
+//     </li>
+//   );
+// }
+
 function App() {
   return (
     <div>
